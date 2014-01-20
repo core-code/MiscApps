c0f6671875a9c33b37cf625f36c84954f7f0ab67
Julian Mayer <julian@corecode.at>
2014-01-20 17:36:56 +0100

    update translator

--- Translator/Translator/AppDelegate.m
+++ Translator/Translator/AppDelegate.m
@@ -3,8 +3,11 @@
 //  Translator
 //
 //  Created by corecodeon 20.01.14.
-//  Copyright (c) 2014 corecode. All rights reserved.
-//
+/*	Copyright (c) 2014 CoreCode
+ Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitationthe rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+ The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
 
 #import "AppDelegate.h"
 
@@ -61,11 +64,10 @@
 		assert([file writeToURL:[[[url URLByDeletingLastPathComponent] URLByAppendingPathComponent:translationName] URLByAppendingPathComponent:@"MainMenu.strings"] atomically:NO encoding:NSUTF8StringEncoding error:nil]);
 
 	}
-
-
-
-
-	
 }
-
 @end
+
+int main(int argc, const char * argv[])
+{
+	return NSApplicationMain(argc, argv);
+}
