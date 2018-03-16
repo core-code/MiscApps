2f811e7f1655694fda99984b570116c436bbacd1
Julian Mayer <julian@corecode.at>
2018-03-16 15:41:50 +0100

    new project 'observefreediskspace' and fix readme

---
+++ ObserveFreeDiskSpace/observeFreeDiskSpace/main.m
@@ -0,0 +1,36 @@
+//
+//  main.m
+//  observeFreeDiskSpace
+//
+//  Created by CoreCode on 16/03/2018.
+/*    Copyright © 2018 CoreCode Limited
+ Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitationthe rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+ The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+
+#import <Foundation/Foundation.h>
+#import "CoreLib.h"
+
+
+int main(int argc, const char * argv[])
+{
+    @autoreleasepool
+    {
+        while (1)
+        {
+            NSURL *fileURL = [NSURL fileURLWithPath:@"/"];
+            
+            NSDictionary *values = [fileURL resourceValuesForKeys:@[NSURLVolumeAvailableCapacityForImportantUsageKey, NSURLVolumeAvailableCapacityForOpportunisticUsageKey]
+                                              error:NULL];
+            
+            double free1 = BYTES_TO_MB_NEW([values[@"NSURLVolumeAvailableCapacityForImportantUsageKey"] doubleValue]);
+            double free2 = BYTES_TO_MB_NEW([values[@"NSURLVolumeAvailableCapacityForOpportunisticUsageKey"] doubleValue]);
+
+            
+            cc_log(@"volumeAvailableCapacityForImportantUsage %.2f volumeAvailableCapacityForOpportunisticUsage %.2f", free1, free2);
+            sleep(1);
+        }
+    }
+    return 0;
+}
