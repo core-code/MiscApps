3b4eac36a7c5ffc9b6045bb2073c1a9f81dc53c4
Julian Mayer <julian@corecode.io>
2020-03-23 21:40:53 +0100

    minor

--- DMGUncompresser/DMGUncompresser/AppDelegate.m
+++ DMGUncompresser/DMGUncompresser/AppDelegate.m
@@ -31,6 +31,7 @@
     }
     
     [self displayUserNotification:@"DMG Decompression Finished" text:@"Finito"];
+    cc_log(@"DONE with all files");
 }
 
 - (void)displayUserNotification:(NSString *)title text:(NSString *)text
